a68ffa8c75d0ef08445a67d2cb139cff43402dc5
BhaskarDileep27 <bhaskardileep27@gmail.com>
2026-07-08 13:00:47 +0530

    small conversation

--- shell-script/04-variable.sh
+++ shell-script/04-variable.sh
@@ -2,11 +2,11 @@
 persion1=$1
 persion2=$2
 
-persion1: hai persion2, how are you
-persion2: hai persion1, i am fine what about you
-persion1: I am also fine thank you, what are you doing now
-persion2: I am learning shell scripting, what about you
-persion1: i am also learning shell scripting, but i am facing some issues in it
-persion2: what issues you are facing i am also learning shell scripting, maybe I can be able to he lp you
-persion1: I am facing issues in variable declaration and usage, can you please he lp me in it
-persion2: yes sure, I can help you in it, what issues you are facing in variable declaration an d usage
+echo $persion1: hai $persion2, how are you
+echo $persion2: hai $persion1, i am fine what about you
+echo $persion1: I am also fine thank you, what are you doing now
+echo $persion2: I am learning shell scripting, what about you
+echo $persion1: i am also learning shell scripting, but i am facing some issues in it
+echo $persion2: what issues you are facing i am also learning shell scripting, maybe I can be able to he lp you
+echo $persion1: I am facing issues in variable declaration and usage, can you please he lp me in it
+echo $persion2: yes sure, I can help you in it, what issues you are facing in variable declaration an d usage
